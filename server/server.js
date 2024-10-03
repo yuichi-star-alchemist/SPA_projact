02a2a4dcff4c291e8b5878937778b5c98f937197
ゆういち <159900048+yuichi-star-alchemist@users.noreply.github.com>
2024-10-03 16:07:01 +0900

    サーバー起動 + ルート設定

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+const publicPath = path.join(`${__dirname.slice(0, -6)}client/public/`);// dirname末尾のserverは削除
 
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(`${publicPath}index.html`);
+});
+
+app.get('/nonogram', (req, res) => {
+  res.sendFile(`${publicPath}nonogram.html`);
 });
 
 
