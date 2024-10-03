7120e128e896631e22b7955dab9013fa8489a487
ゆういち <159900048+yuichi-star-alchemist@users.noreply.github.com>
2024-10-03 16:48:59 +0900

    server.jsにコメント追加

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const publicPath = path.join(`${__dirname.slice(0, -6)}client/public/`);// dirname末尾のserverは削除
+// dirname末尾のserverは削除
+// sendFileでは..の使用は制限されているので上から行く
+const publicPath = path.join(`${__dirname.slice(0, -6)}client/public/`);
 
+// 静的ファイルへのアクセスが可能な領域を指定する必要がある
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
