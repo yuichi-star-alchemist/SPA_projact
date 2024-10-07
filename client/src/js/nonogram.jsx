472577730a338ce32b9ca44734484c74650e847f
ゆういち <159900048+yuichi-star-alchemist@users.noreply.github.com>
2024-10-07 16:59:49 +0900

    [feature] label部にカウント初期表示

--- client/src/js/nonogram.jsx
+++ client/src/js/nonogram.jsx
@@ -7,19 +7,32 @@ const cells = 20
 
 function Game() {
   let content
+  /* フィールドの構造
+  0  1  2...  |cells==20
+  20 21 22
+  .
+  .
+  380......cells**2-1
+  -----------------*/
   const [isGame, setIsGame] = useState(false)
+  // 当たり判定を保持
   const [fieldBoolList, setFieldBoolList] = useState(Array(cells ** 2).fill())
+  // クリックフラグを保持
   const [fieldStateList, setFieldStateList] = useState(Array(cells ** 2).fill(false))
+  // row, column 連続数を保持
+  const [labelsCountList, setLabelsCountList] = useState()
 
   if (isGame) {
-    content = <Board handleUpdateFieldStateList={ onUpdateFieldStateList } fieldStateList={ fieldStateList } fieldBoolList={ fieldBoolList } />
+    content = <Board handleUpdateFieldStateList={ onUpdateFieldStateList } fieldStateList={ fieldStateList } fieldBoolList={ fieldBoolList } labelsCountList={ labelsCountList } />
   } else {
     content = <GameInit onInitGame={ handleInitGame } />
   }
 
   function handleInitGame(difficultyIndex) {
-    setFieldBoolList(createFieldBoolList(difficultyIndex))
-    setIsGame(!isGame)
+    const newFieldBoolList = createFieldBoolList(difficultyIndex)
+    setFieldBoolList(newFieldBoolList)
+    setLabelsCountList(makeCountList(newFieldBoolList))
+    setIsGame(true)
   }
   
   function createFieldBoolList(difficultyIndex) {
@@ -38,6 +51,41 @@ function Game() {
     return newFieldList
   }
 
+  function getCountInColumn(newFieldBoolList, i, j) {
+    const result = []
+    let count = 0
+    let idx
+    for (let k=0; k<cells; k++) {
+      if (i == 0) {// rowは左からj行目
+        idx = cells * k + j
+      } else {// columnは上からj列目
+        idx = cells * j + k
+      }
+      if (newFieldBoolList[idx]) {
+        count++
+      } else {
+        if (count) {
+          result.push(count)
+          count = 0
+        }
+      }
+      if (k == cells - 1 && count) result.push(count)
+    }
+    
+    return result
+  }
+
+  function makeCountList(newFieldBoolList) {
+    const countList = [[], []]
+    for (let i=0; i<2; i++) {
+      for (let j=0; j<cells; j++) {
+        countList[i].push(getCountInColumn(newFieldBoolList, i, j))
+      }
+    }
+
+    return countList
+  }
+
   function onUpdateFieldStateList(idx) {
     if (fieldStateList[idx]) return
 
@@ -47,9 +95,17 @@ function Game() {
     setFieldStateList(newFieldStateList)
   }
 
+  function checkIsTrueInColumn() {
+    // 渡されたindexの1列の中にtrueがあるかどうかの判定を返す
+    return
+  }
+
+
+
   return (
     <ContainerDiv addClass="content">
       <GameHeader />
+      {/* <button onClick={ () => console.log(labelsCountList) }></button> */}
       { content }
     </ContainerDiv>
   )
@@ -72,7 +128,7 @@ function SettingsButton() {
 }
 
 function RestartButton() {
-  return <OnClickButton onClick={ () => {} }>もう一度！</OnClickButton>
+  return <OnClickButton className="restart" onClick={ () => {} }>もう一度！</OnClickButton>
 }
 
 function GameEndView() {
@@ -109,7 +165,7 @@ function Instructions() {
   )
 }
 
-function Board({ handleUpdateFieldStateList, fieldStateList, fieldBoolList }) {
+function Board({ handleUpdateFieldStateList, fieldStateList, fieldBoolList, labelsCountList }) {
   return (
     <ContainerDiv addClass="board">
       <ContainerDiv addClass="board-top">
@@ -118,10 +174,10 @@ function Board({ handleUpdateFieldStateList, fieldStateList, fieldBoolList }) {
           <MarkView />
           <TimeView />
         </ContainerDiv>
-        <LabelContainer className="row" />
+        <LabelContainer className="row" labelsCountList={ labelsCountList } />
       </ContainerDiv>
       <ContainerDiv addClass="board-buttom">
-        <LabelContainer className="column" />
+        <LabelContainer className="column" labelsCountList={ labelsCountList } />
         <CellContainer className="cell-container" handleUpdateFieldStateList={ handleUpdateFieldStateList } fieldStateList={ fieldStateList } fieldBoolList={ fieldBoolList } />
       </ContainerDiv>
     </ContainerDiv>
@@ -153,10 +209,15 @@ function TimeView() {
   return time
 }
 
-function LabelContainer({ className }) {
+function LabelContainer({ className, labelsCountList }) {
   const labelList = Array(cells).fill().map((val, idx) => {
-    return <Label key={ idx } />
+    return <Label key={ idx } labelCountList={ makeLabelCountList(idx) } />
   })
+
+  function makeLabelCountList(idx) {
+    const isRow = className == "row" ? 0 : 1
+    return [...labelsCountList[isRow][idx]].reverse()
+  }
 
   return (
     <ContainerDiv addClass={ className }>
@@ -165,18 +226,14 @@ function LabelContainer({ className }) {
   )
 }
 
-function Label() {
+function Label({ labelCountList }) {
+  const labelCount = labelCountList.map((val, idx) => {
+    return <div key={ idx }>{ val }</div>
+  })
+
   return (
     <ContainerDiv addClass="label">
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>10</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
+      { labelCount }
     </ContainerDiv>
   )
 }
